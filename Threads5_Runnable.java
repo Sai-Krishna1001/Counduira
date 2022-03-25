ddab3602e178de85936c9bc6e3c45529e0800c04
Sai-Krishna1001 <101165271+Sai-Krishna1001@users.noreply.github.com>
2022-03-25 11:18:51 +0530

    Add files via upload

---
+++ Threads5_Runnable.java
@@ -0,0 +1,28 @@
+class MyThread implements Runnable{
+    
+    @Override
+    public void run(){
+        System.out.println("@@thread name : " + Thread.currentThread().getName());
+    }
+}
+
+public class Threads5_Runnable
+{
+	public static void main(String[] args) {
+		MyThread thread1 = new MyThread();
+		Thread  t1 = new Thread(thread1);
+		t1.start();
+		MyThread thread2 = new MyThread();
+		Thread  t2 = new Thread(thread2);
+		t2.start();
+		MyThread thread3 = new MyThread();
+		Thread  t3 = new Thread(thread3);
+		t3.start();
+		MyThread thread4 = new MyThread();
+		Thread  t4 = new Thread(thread4);
+		t4.start();
+		MyThread thread5 = new MyThread();
+		Thread  t5 = new Thread(thread5);
+		t5.start();
+	}
+}
